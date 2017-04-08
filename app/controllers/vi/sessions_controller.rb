d5fde628e3a2d7271c81a17290c625873a103eff
KoichiOzaki <goingmyway19851224@gmail.com>
2017-04-08 14:34:50 +0900

    The SessionSerializer is an Active Model Serializer object

--- app/controllers/vi/sessions_controller.rb
+++ app/controllers/vi/sessions_controller.rb
@@ -1,23 +1,25 @@
-class Vi::SessionsController < ApplicationController
-  skip_before_action :authenticate_user_from_token!
+module V1
+  class SessionsController < ApplicationController
+    skip_before_action :authenticate_user_from_token!
 
-  # POST /v1/login
-  def create
-    @user = User.find_for_database_authentication(email: params[:username])
-    return invalid_login_attempt unless @user
+    # POST /v1/login
+    def create
+      @user = User.find_for_database_authentication(email: params[:username])
+      return invalid_login_attempt unless @user
 
-    if @user.valid_password?(params[:password])
-      sign_in :user, @user
-      render json: @user, serializer: SessionSerializer, root: nil
-    else
-      invalid_login_attempt
+      if @user.valid_password?(params[:password])
+        sign_in :user, @user
+        render json: @user, serializer: SessionSerializer, root: nil
+      else
+        invalid_login_attempt
+      end
     end
-  end
 
-  private
+    private
 
-  def invalid_login_attempt
-    warden.custom_failure!
-    render json: {error: t('sessions_controller.invalid_login_attempt')}, status: :unprocessable_entity
+    def invalid_login_attempt
+      warden.custom_failure!
+      render json: {error: t('sessions_controller.invalid_login_attempt')}, status: :unprocessable_entity
+    end
   end
 end
