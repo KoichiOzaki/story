8cbd110a45c46f202fdbba862b71f811d21c261c
KoichiOzaki <goingmyway19851224@gmail.com>
2017-04-08 14:31:49 +0900

    The user authentication will sit in the application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :trackable, :validatable
   devise :database_authenticatable, :recoverable, :validatable
+
+  after_create :update_access_token!
+
+  validates :username, presence: true
+  validates :email, presence: true
+
+  private
+
+  def update_access_token!
+    self.access_token = "#{self.id}:#{Devise.friendly_token}"
+    save
+  end
 end
